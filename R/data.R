cd3da9af45af042926774272ee3f3f7c2d578fdc
Simon Shamus Riley <au802896@uni.au.dk>
2025-12-19 11:48:00 +0100

    Draft vignette on marginal means and contrasts

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Fodder Beet Yields
+#'
+#' Data on fodder beet yields for use in class contrasts example.
+#'
+#' @format ## `greenfeeds`
+#' A data frame with 32 rows and 3 columns:
+#' \describe{
+#'   \item{Treatment}{Factor. Beet variety.}
+#'   \item{Rep}{Factor. The replicate number.}
+#'   \item{Yield}{Numeric. Beet yield in tons/ha}
+#'   ...
+#' }
+#' @source Saville, D. J., & Wood, G. R. (1991). Statistical Methods: The Geometric Approach. Springer Science & Business Media.
+"greenfeeds"
+
+#' Effect of Nitrolime and Superphosphate on Barley Yields
+#'
+#' Data on barley yield for use in interaction contrast example.
+#'
+#' @format ## `barley`
+#' A data frame with 20 rows and 4 columns:
+#' \describe{
+#'   \item{Rep}{Factor. The replicate number}
+#'   \item{Nitrolime}{Factor. The nitrolime application rate.}
+#'   \item{Superphosphate}{Factor. The superphosphate application rate.}
+#'   \item{Yield}{Numeric. The yield in kg/ha.}
+#' }
+#' @source Saville, D. J., & Wood, G. R. (1991). Statistical Methods: The Geometric Approach. Springer Science & Business Media.
+"barley"
